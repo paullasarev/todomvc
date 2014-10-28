ce4b0512973c9dd021460908ac6d16aa80a72386
paullasarev <paullasarev@gmail.com>
2014-10-28 07:21:57 +0400

    add tdstyle

--- test/css.js
+++ test/css.js
@@ -1,6 +1,6 @@
 describe("css", function(){
   var expect = window.chai.expect;
-  var ctyle = window.cstyle;
+  var tdstyle = window.tdstyle;
 
   var bodyBackgroundColor = '#e0e0e0';
   var headerTitleTextColor = '#c7c7c7';
@@ -18,53 +18,53 @@ describe("css", function(){
 
   describe("body", function(){
     it('body should be clean', function() {
-      expect(cstyle.isContentOnly('body')).ok;
+      expect(tdstyle.isContentOnly('body')).ok;
     });
   });
 
   describe(".todo", function(){
     it('todo should be centered', function() {
-      expect(cstyle.isHorisontallyCentered('body', '.todo')).ok;
+      expect(tdstyle.isHorisontallyCentered('body', '.todo')).ok;
     });
 
     it('todo should be onTop', function() {
-      expect(cstyle.isOnTop('body', '.todo')).ok;
+      expect(tdstyle.isOnTop('body', '.todo')).ok;
     });
 
     it('todo should have fixed width', function() {
-      expect(cstyle.getWidth('.todo')).equal(todoWidth);
+      expect(tdstyle.getWidth('.todo')).equal(todoWidth);
     });
 
     it('todo background color', function() {
-      expect(cstyle.isColor('.todo', 'background-color', bodyBackgroundColor));
+      expect(tdstyle.isColor('.todo', 'background-color', bodyBackgroundColor));
     });
   });
 
   describe(".todo__header", function(){
     it('header should be centered', function() {
-      expect(cstyle.isHorisontallyCentered('.todo', '.todo__header')).ok;
+      expect(tdstyle.isHorisontallyCentered('.todo', '.todo__header')).ok;
     });
   });
 
   describe(".todo__title", function(){
     it('should be 100px height', function() {
-      expect(cstyle.getHeight('.todo__title')).equal(100);
+      expect(tdstyle.getHeight('.todo__title')).equal(100);
     });
 
     it('should be "todos"', function() {
-      expect(cstyle.getText('.todo__title')).equal('todos');
+      expect(tdstyle.getText('.todo__title')).equal('todos');
     });
 
     it('should be centered', function() {
-      expect(cstyle.isCentered('.todo__header', '.todo__title')).ok;
+      expect(tdstyle.isCentered('.todo__header', '.todo__title')).ok;
     });
 
     it('should be only content', function() {
-      expect(cstyle.isContentOnly('.todo__title')).ok;
+      expect(tdstyle.isContentOnly('.todo__title')).ok;
     });
 
     it('text should be bold fontface', function() {
-      cstyle.isFont('.todo__title', {
+      tdstyle.isFont('.todo__title', {
         size: "70px",
         weight: "bold",
         family: "sans-serif",
@@ -73,58 +73,58 @@ describe("css", function(){
     });
 
     it('text color', function() {
-      cstyle.isColor('.todo__title', 'color', headerTitleTextColor);
+      tdstyle.isColor('.todo__title', 'color', headerTitleTextColor);
     });
 
     //text-shadow: -1px -1px rgba(0, 0, 0, 0.2);
     it('should have shadow', function() {
-      cstyle.haveProperty('.todo__title', 'text-shadow');
+      tdstyle.haveProperty('.todo__title', 'text-shadow');
     });
 
   });
 
   describe(".todo__caption", function(){
     it('should fit after header todo', function() {
-      cstyle.isUnder('.todo__header', '.todo__caption');
-      cstyle.isFitWidth('.todo', '.todo__caption');
+      tdstyle.isUnder('.todo__header', '.todo__caption');
+      tdstyle.isFitWidth('.todo', '.todo__caption');
     });
 
     it('should have splitter', function() {
-      cstyle.isOnTop('.todo__caption', '.todo__splitter');
-      cstyle.isFitWidth('.todo', '.todo__splitter');
-      cstyle.isHeight('.todo__splitter', splitterHeight);
-      cstyle.isColor('.todo__splitter', 'background-color', splitterColor);
+      tdstyle.isOnTop('.todo__caption', '.todo__splitter');
+      tdstyle.isFitWidth('.todo', '.todo__splitter');
+      tdstyle.isHeight('.todo__splitter', splitterHeight);
+      tdstyle.isColor('.todo__splitter', 'background-color', splitterColor);
      });
     
     it('should have input', function() {
-      cstyle.isUnder('.todo__splitter', '.todo__input');
-      cstyle.isFitWidth('.todo', '.todo__input');
-      cstyle.isTag('.todo__input', 'input');
+      tdstyle.isUnder('.todo__splitter', '.todo__input');
+      tdstyle.isFitWidth('.todo', '.todo__input');
+      tdstyle.isTag('.todo__input', 'input');
     });
 
     it('should fix input parameters', function() {
-      cstyle.isHeight('.todo__input', inputHeight);
-      cstyle.isBox('.todo__input', {
+      tdstyle.isHeight('.todo__input', inputHeight);
+      tdstyle.isBox('.todo__input', {
         paddingLeft: inputCheckWidth,
         paddingRight: inputDelWidth,
         border: "none",
         margin: 0,
       });
 
-      cstyle.isFont('.todo__input', {
+      tdstyle.isFont('.todo__input', {
         size: inputFontSize,
         style: "normal",
         family: "sans-serif",
       });
 
-      cstyle.isColor('.todo__input', 'background-color', inputBackgroundColor);
+      tdstyle.isColor('.todo__input', 'background-color', inputBackgroundColor);
 
-      cstyle.isAttribute('.todo__input', 'placeholder', 'What needs to be done?');
-      cstyle.isTextVCentered('.todo__input');
+      tdstyle.isAttribute('.todo__input', 'placeholder', 'What needs to be done?');
+      tdstyle.isTextVCentered('.todo__input');
     });
       
     it('input placeholder font should be italic', function() {
-      cstyle.isFont('.todo__input:placeholder',  {
+      tdstyle.isFont('.todo__input:placeholder',  {
         style: 'italic',
         weight: '200',
         color: '#999999',
@@ -132,7 +132,7 @@ describe("css", function(){
     });
 
     it('should have dotted bottom', function() {
-      cstyle.isBox('.todo__caption', {
+      tdstyle.isBox('.todo__caption', {
         'border-bottom': "dotted 1px #ccc",
       });
     });
@@ -142,38 +142,38 @@ describe("css", function(){
   describe(".todo__list", function(){
     var sel = '.todo__list';
     it('should have item', function() {
-      cstyle.isUnder('.todo__caption', sel);
-      cstyle.isFitWidth('.todo', sel);
-      cstyle.isTag(sel, 'div');
+      tdstyle.isUnder('.todo__caption', sel);
+      tdstyle.isFitWidth('.todo', sel);
+      tdstyle.isTag(sel, 'div');
     });
   });
 
   describe(".todo__item", function(){
     var sel = '.todo__item';
     it('should have item', function() {
-      cstyle.isOnTop('.todo__list', sel);
-      cstyle.isFitWidth('.todo', sel);
-      cstyle.isTag(sel, 'div');
+      tdstyle.isOnTop('.todo__list', sel);
+      tdstyle.isFitWidth('.todo', sel);
+      tdstyle.isTag(sel, 'div');
     });
 
     it('should fix parameters', function() {
-      cstyle.isHeight(sel, inputHeight);
-      cstyle.isBox(sel, {
+      tdstyle.isHeight(sel, inputHeight);
+      tdstyle.isBox(sel, {
         'border-left': "none",
         'border-right': "none",
         margin: 0,
       });
-      cstyle.isFont(sel, {
+      tdstyle.isFont(sel, {
         size: inputFontSize,
         style: "normal",
         family: "sans-serif",
       });
 
-      cstyle.isColor(sel, 'background-color', inputBackgroundColor);
+      tdstyle.isColor(sel, 'background-color', inputBackgroundColor);
     });
 
     it('should have dotted bottom', function() {
-      cstyle.isBox(sel, {
+      tdstyle.isBox(sel, {
         'border-bottom': "dotted 1px #ccc",
       });
     });
@@ -181,21 +181,21 @@ describe("css", function(){
 
     it('should have check button', function() {
       var sel = '.todo__item__check';
-      cstyle.isTag(sel, 'input');
-      cstyle.isAttribute(sel, 'type', 'checkbox');
-      cstyle.isBox(sel, {
+      tdstyle.isTag(sel, 'input');
+      tdstyle.isAttribute(sel, 'type', 'checkbox');
+      tdstyle.isBox(sel, {
         width: inputCheckWidth,
         color: inputBackgroundColor,
         border: 'none',
       });
-      cstyle.isFitHeight('.todo__item', sel);
-      cstyle.isStartOnLeft('.todo__item', sel);
+      tdstyle.isFitHeight('.todo__item', sel);
+      tdstyle.isStartOnLeft('.todo__item', sel);
     });
 
     it('should have splitter', function() {
       var sel = '.todo__item__splitter';
-      cstyle.isTag(sel, 'div');
-      cstyle.isBox(sel, {
+      tdstyle.isTag(sel, 'div');
+      tdstyle.isBox(sel, {
         width: itemSplitterWidth,
         color: inputBackgroundColor,
         borderLeft: 'solid 1px red',
@@ -203,22 +203,22 @@ describe("css", function(){
         borderTop: 'none',
         borderBottom: 'none',
       });
-      cstyle.isFitHeight('.todo__item', sel);
-      cstyle.isOnRight('.todo__item__check', sel);
+      tdstyle.isFitHeight('.todo__item', sel);
+      tdstyle.isOnRight('.todo__item__check', sel);
     });
 
     it('should have value', function() {
       var sel = '.todo__item__value';
-      cstyle.isTag(sel, 'div');
-      cstyle.isBox(sel, {
+      tdstyle.isTag(sel, 'div');
+      tdstyle.isBox(sel, {
         color: inputBackgroundColor,
         width: todoWidth - inputDelWidth - inputCheckWidth - itemSplitterWidth,
         border: 'none',
         paddingLeft: 15,
       });
-      cstyle.isFitHeight('.todo__item', sel);
-      cstyle.isOnRight('.todo__item__splitter', sel);
-      cstyle.isTextVCentered(sel);
+      tdstyle.isFitHeight('.todo__item', sel);
+      tdstyle.isOnRight('.todo__item__splitter', sel);
+      tdstyle.isTextVCentered(sel);
     });
 
   });
